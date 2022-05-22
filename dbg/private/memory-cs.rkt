4b057c9e126328f3920c7d6e1565d1da8f6762e4
Bogdan Popa <bogdan@defn.io>
2022-05-22 10:29:33 +0300

    server,memory-cs: escape early during ->string

--- dbg/private/memory-cs.rkt
+++ dbg/private/memory-cs.rkt
@@ -18,77 +18,76 @@
 (define record-rtd (vm-primitive 'record-rtd))
 (define record-type-name (vm-primitive 'record-type-name))
 
-(define (call-with-counts-enabled proc)
+(define (call-with-counts proc)
   (call-as-atomic
    (lambda ()
      (enable-object-counts #t)
      (enable-object-backreferences #t)
      (collect-garbage)
-     (begin0 (proc)
-       (enable-object-counts #f)
-       (enable-object-backreferences #f)))))
-
-(define (get-counts&backrefs)
-  (call-with-counts-enabled
-   (lambda ()
-     (values
-      (object-counts)
-      (object-backreferences)))))
+     (define counts (object-counts))
+     (define backrefs (object-backreferences))
+     (enable-object-counts #f)
+     (enable-object-backreferences #f)
+     (proc counts backrefs))))
 
 (define (get-object-counts)
-  (define-values (counts _backrefs)
-    (get-counts&backrefs))
   (define counts-by-str
-    (for*/fold ([res (hash)])
-               ([c (in-list counts)]
-                [str (in-value (->string (car c)))]
-                [gen (in-list (cdr c))])
-      (hash-update res str
-                   (λ (cnt)
-                     (cons
-                      (+ (cadr gen) (car cnt))
-                      (+ (cddr gen) (cdr cnt))))
-                   (cons 0 0))))
+    (call-with-counts
+     (lambda (counts _backrefs)
+       (for*/fold ([res (hash)])
+                  ([c (in-list counts)]
+                   [str (in-value (->string (car c)))]
+                   [gen (in-list (cdr c))])
+         (hash-update res str
+                      (λ (cnt)
+                        (cons
+                         (+ (cadr gen) (car cnt))
+                         (+ (cddr gen) (cdr cnt))))
+                      (cons 0 0))))))
   (sort (hash-map counts-by-str cons) #:key cddr >))
 
 (define (get-object-graph proc)
-  (define-values (_counts backrefs)
-    (get-counts&backrefs))
-  (define id-seq 0)
-  (define ids (make-hasheq))
-  (define (id ob)
-    (hash-ref! ids ob (lambda ()
-                        (begin0 id-seq
-                          (set! id-seq (add1 id-seq))))))
-  (define graph
-    (make-hasheq))
-  (for* ([gen-backrefs (in-list backrefs)]
-         [backref (in-list gen-backrefs)])
-    (define ob (car backref))
-    (define ref (cdr backref))
-    (hash-update!
-     graph ob
-     (lambda (refs)
-       (cons ref refs))
-     null))
   (define metadata (make-hasheqv))
   (define links (make-hasheqv))
-  (define (track! ob)
-    (define ob-id (id ob))
-    (unless (hash-has-key? metadata ob-id)
-      (hash-set! metadata ob-id (object-metadata ob ob-id))
-      (for ([ref (in-list (hash-ref graph ob null))])
-        (define ref-id (id ref))
-        (hash-update! links ob-id
-                      (λ (ob-ids)
-                        (cons ref-id ob-ids))
-                      null))
-      (for ([ref (in-list (hash-ref graph ob null))])
-        (track! ref))))
   (define objects
-    (for/list ([ob (in-hash-keys graph)] #:when (proc ob))
-      (track! ob)
-      (id ob)))
+    (call-with-counts
+     (lambda (_counts backrefs)
+       (define id-seq 0)
+       (define ids (make-hasheq))
+       (define (id ob)
+         (hash-ref! ids ob (lambda ()
+                             (begin0 id-seq
+                               (set! id-seq (add1 id-seq))))))
+
+
+       (define graph (make-hasheq))
+       (for* ([gen-backrefs (in-list backrefs)]
+              [backref (in-list gen-backrefs)])
+         (define ob (car backref))
+         (define ref (cdr backref))
+         (hash-update!
+          graph ob
+          (lambda (refs)
+            (cons ref refs))
+          null))
+
+       (define (track! ob)
+         (define ob-id (id ob))
+         (unless (hash-has-key? metadata ob-id)
+           (hash-set! metadata ob-id (object-metadata ob ob-id))
+           (for ([ref (in-list (hash-ref graph ob null))])
+             (define ref-id (id ref))
+             (hash-update! links ob-id
+                           (λ (ob-ids)
+                             (cons ref-id ob-ids))
+                           null))
+           (for ([ref (in-list (hash-ref graph ob null))])
+             (track! ref))))
+
+       (for/list ([ob (in-hash-keys graph)] #:when (proc ob))
+         (track! ob)
+         (id ob)))))
+
   (hasheq
    'metadata metadata
    'objects objects
@@ -103,10 +102,15 @@
 (define (get-object-graph/by-type type)
   (define pred
     (case type
+      [(bignum) (vm-primitive 'bignum?)]
+      [(box) (vm-primitive 'box?)]
       [(bytevector) (vm-primitive 'bytevector?)]
+      [(ephemeron) (vm-primitive 'ephemeron?)]
       [(pair) (vm-primitive 'pair?)]
       [(procedure) (vm-primitive 'procedure?)]
       [(string) (vm-primitive 'string?)]
+      [(symbol) (vm-primitive 'symbol?)]
+      [(thread) thread?]
       [(vector) (vm-primitive 'vector?)]
       [else (λ (_) #f)]))
 
@@ -114,9 +118,9 @@
 
 (define (->string ob [max-length #f])
   (define str
-    (call-with-output-string
-     (lambda (out)
-       (write ob out))))
+    (if max-length
+        (call-with-limited-output-string max-length (λ (out) (write ob out)))
+        (call-with-output-string (λ (out) (write ob out)))))
   (cond
     [(and max-length (> (string-length str) max-length))
      (string-set! str (sub1 max-length) #\…)
@@ -135,3 +139,33 @@
    'str (->string ob 255)
    'name (and maybe-name (->string maybe-name 255))
    'hash (equal-hash-code ob)))
+
+(define (call-with-limited-output-string cap proc)
+  (define len 0)
+  (define out-bs (make-bytes cap))
+  (define res-bs
+    (let/ec esc
+      (define out
+        (make-output-port
+         "limited"
+         always-evt
+         (lambda (data start end _dont-block? _enable-breaks?)
+           (define remaining
+             (- cap len))
+           (define want
+             (- end start))
+           (cond
+             [(zero? remaining)
+              (esc out-bs)]
+             [(< want remaining)
+              (bytes-copy! out-bs len data start end)
+              (set! len (+ len want))
+              want]
+             [else
+              (bytes-copy! out-bs len data start (+ start remaining))
+              (set! len (+ len remaining))
+              remaining]))
+         void))
+      (proc out)
+      (subbytes out-bs 0 len)))
+  (bytes->string/utf-8 res-bs #\�))
