5335017eb93509996afcfcd3078f1c53b58f6528
Bogdan Popa <bogdan@defn.io>
2025-06-04 08:34:57 +0300

    core,build: bump version

--- dbg/info.rkt
+++ dbg/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.4.1")
+(define version "0.5")
 (define collection "debugging")
 (define deps '("base"
                "profile-lib"))
